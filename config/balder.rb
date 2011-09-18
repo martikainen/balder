f9d84786f647d893dca0eaad9e109717a9aedd0e
Magnus Martikainen <magnus@rudat-martikainen.com>
2011-09-18 15:35:00 -0700

    enabled private, changed copyright notice, updated gems, changed title

--- config/balder.rb
+++ config/balder.rb
@@ -1,6 +1,6 @@
 ENV['STORAGE_PATH']       = "uploads" if ENV['STORAGE_PATH'].nil?
-#ENV['PRIVATE']           = 'true'
-ENV['TITLE']            = 'photo'
+ENV['PRIVATE']           = 'true'
+ENV['TITLE']            = 'Rudat-Martikainen'
 ENV['HEROKU']           = 'true'
 ENV['S3_KEY']           = 'AKIAIEOKBNNUZ262B74A'
 ENV['S3_SECRET']        = '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'
